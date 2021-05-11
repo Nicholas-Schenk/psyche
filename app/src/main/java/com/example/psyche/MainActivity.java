e1f89d3f8f48fcfe30e27b55297f7890598b75c7
Nick <nicholas.schenk@rutgers.edu>
2021-05-11 12:49:31 -0400

    start

---
+++ app/src/main/java/com/example/psyche/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.psyche;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        //temp
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_main);
+
+    }
+}
