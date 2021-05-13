da3d77c5fa2e88e70ac1e6fde72f6ccee47ecd88
Nick <nicholas.schenk@rutgers.edu>
2021-05-13 13:07:36 -0400

    fixed questionnaire, added selected goal, started profile

--- app/src/main/java/com/example/psyche/Profile.java
+++ app/src/main/java/com/example/psyche/Profile.java
@@ -1,6 +1,5 @@
 package com.example.psyche;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -10,31 +9,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
-import android.widget.Button;
 
-public class Questionnaire extends AppCompatActivity {
-    Button continueButton;
+public class Profile extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_questionnaire);
+        setContentView(R.layout.activity_profile);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-
-        continueButton = findViewById(R.id.continue_button);
-        continueButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                nextScreen();
-            }
-        });
-    }
-
-    public void nextScreen() {
-        Intent intent = new Intent(this, SelectGoal.class);
-        startActivity(intent);
     }
 }
