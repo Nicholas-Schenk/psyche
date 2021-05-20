10997c394c3a2eed7bfc90e1dd7eee91bb03590a
Nick <nicholas.schenk@rutgers.edu>
2021-05-20 12:56:33 -0400

    work on homescreen

--- app/src/main/java/com/example/psyche/Profile.java
+++ app/src/main/java/com/example/psyche/Profile.java
@@ -1,5 +1,6 @@
 package com.example.psyche;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -9,9 +10,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.Button;
 
 public class Profile extends AppCompatActivity {
-
+    Button beginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,5 +21,19 @@ public class Profile extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        beginButton = findViewById(R.id.begin_button);
+        beginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                nextScreen();
+            }
+        });
     }
+
+    public void nextScreen(){
+        Intent intent = new Intent(this, HomeScreen.class);
+        startActivity(intent);
+    }
+
 }
